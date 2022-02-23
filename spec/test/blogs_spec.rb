7b7c05e3d034432b93a3aafa4a426cc63e04b3ff
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-23 20:10:50 +0100

    Add test to check the response status code for blog show method

--- spec/test/blogs_spec.rb
+++ spec/test/blogs_spec.rb
@@ -13,6 +13,13 @@ RSpec.describe 'Blogs', type: :request do
     it "Should has a correct placeholder text" do
       expect(response.body).to include('Posts')
     end
+  end
 
+  describe 'GET #show' do
+    before(:each) { get blogs_path(2) }  # get(:index)
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+    
   end
 end
