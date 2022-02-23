68887eb26e324c6e0798aaac519c6a82a260351c
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-23 19:15:54 +0100

    Add test to check the response status code for user controller

---
+++ spec/test/users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    before(:each) { get users_path }  # get(:index)
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+    
+  end
+end
