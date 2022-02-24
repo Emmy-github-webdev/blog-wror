5dc552268637c99e87daddfac3c994e8dd959bc9
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-24 17:07:42 +0100

    Run dd:migarte to create the relationship between user and comment schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_24_155544) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_24_160518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_24_155544) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_24_155544) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
 end
