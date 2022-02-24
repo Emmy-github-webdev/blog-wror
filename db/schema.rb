07f513105f92cb52f164f22b32936d8f4f9b460a
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-24 18:29:01 +0100

    Fix linter error

--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_24_171313) do
+ActiveRecord::Schema[7.0].define(version: 20_220_224_171_313) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "like_id"
-    t.index ["like_id"], name: "index_blogs_on_like_id"
-    t.index ["user_id"], name: "index_blogs_on_user_id"
+  create_table 'blogs', force: :cascade do |t|
+    t.string 'title'
+    t.text 'text'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id'
+    t.bigint 'like_id'
+    t.index ['like_id'], name: 'index_blogs_on_like_id'
+    t.index ['user_id'], name: 'index_blogs_on_user_id'
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.bigint "blog_id", null: false
-    t.index ["blog_id"], name: "index_comments_on_blog_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table 'comments', force: :cascade do |t|
+    t.text 'text'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id', null: false
+    t.bigint 'blog_id', null: false
+    t.index ['blog_id'], name: 'index_comments_on_blog_id'
+    t.index ['user_id'], name: 'index_comments_on_user_id'
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'likes', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.text "photo"
-    t.text "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "like_id"
-    t.index ["like_id"], name: "index_users_on_like_id"
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.text 'photo'
+    t.text 'bio'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'like_id'
+    t.index ['like_id'], name: 'index_users_on_like_id'
   end
 
-  add_foreign_key "blogs", "likes"
-  add_foreign_key "blogs", "users"
-  add_foreign_key "comments", "blogs"
-  add_foreign_key "comments", "users"
-  add_foreign_key "users", "likes"
+  add_foreign_key 'blogs', 'likes'
+  add_foreign_key 'blogs', 'users'
+  add_foreign_key 'comments', 'blogs'
+  add_foreign_key 'comments', 'users'
+  add_foreign_key 'users', 'likes'
 end
