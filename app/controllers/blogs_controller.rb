be77b5dcdf2c165ffe03bfa50231b6564b58f0df
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-01 00:49:36 +0100

    Fix linter error

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,5 @@
 class BlogsController < ApplicationController
-  def index
-    
-  end
+  def index; end
 
-  def show
-    
-  end
-  
+  def show; end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_28_230622) do
+ActiveRecord::Schema[7.0].define(version: 20_220_228_230_622) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_blogs_on_user_id"
+  create_table 'blogs', force: :cascade do |t|
+    t.string 'title'
+    t.text 'text'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_blogs_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.text "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'photo'
+    t.text 'bio'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "blogs", "users"
+  add_foreign_key 'blogs', 'users'
 end
