caeda07c259d1fbbbb5c52ab249cd8bfc3400a1c
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-01 10:07:57 +0100

    Update method to returns 3 most recent post for a given user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def recent_posts(limit = 3)
-    posts.order(created_at: :desc).limit(limit)
+  def recent_posts()
+    posts.order(created_at: :desc).limit(3)
   end
   
 end
