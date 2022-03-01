a11478506efff3fb1d9de2c078fe99e2528f5b6d
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-01 01:18:38 +0100

    Update the unit testing files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :blogs
   resources :users
+  get 'users/:id', to: 'users#show'
+  get 'users', to: 'users#index'
+
+  resources :blogs
+  get 'blogs/:id', to: 'blogs#show'
+  get 'blogs', to: 'blogs#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'blogs#index'
 end
---
+++ spec/test/blogs_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Blogs', type: :request do
+  describe 'GET #index' do
+    before(:each) { get blogs_path } # get(:index)
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+    it 'Should has a correct placeholder text' do
+      expect(response.body).to include('Welcome')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:each) { get blogs_path(2) } # get(:index)
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Should has a correct placeholder text' do
+      expect(response.body).to include('Welcome')
+    end
+  end
+end
