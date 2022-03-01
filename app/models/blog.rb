c762cbf488bf43526bab0d6711df1f18b29c7527
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-01 11:02:00 +0100

    Add method to updates the posts counter for a user

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,11 @@ class Blog < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def update_post(counter)
+    user.update('postsCounter', counter)
+  end
+  
+  
+  
 end
