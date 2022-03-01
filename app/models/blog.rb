cb95d56b68f4df13d9b6dbf907060fc19e0a7c80
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-01 09:59:20 +0100

    Update like  table

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :likes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_01_081719) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_01_084136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_01_081719) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "blog_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_likes_on_blog_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
@@ -44,4 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_01_081719) do
   add_foreign_key "blogs", "users"
   add_foreign_key "comments", "blogs"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "blogs"
+  add_foreign_key "likes", "users"
 end
