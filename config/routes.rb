8c833027ea230c16fa19cb53844dcdd5b4692bf8
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-02 11:44:17 +0100

    Update the router file

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,15 @@ Rails.application.routes.draw do
   get 'users/:id', to: 'users#show'
   get 'users', to: 'users#index'
 
-  resources :blogs
-  get 'blogs/:id', to: 'blogs#show'
-  get 'blogs', to: 'blogs#index'
+
+  #resources :blogs
+  #get 'blogs/:id', to: 'blogs#show'
+  #get 'blogs', to: 'blogs#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'blogs#index'
+  resources :users, only: [:index, :show] do
+    resources :blogs, only: [:index, :show]
+  end
+  root 'users#index'
 end
