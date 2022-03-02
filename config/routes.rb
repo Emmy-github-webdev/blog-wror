b06accc283324faf982a828281237e385820883e
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-02 11:45:42 +0100

    Fix linter error

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,14 @@ Rails.application.routes.draw do
   get 'users/:id', to: 'users#show'
   get 'users', to: 'users#index'
 
-
-  #resources :blogs
-  #get 'blogs/:id', to: 'blogs#show'
-  #get 'blogs', to: 'blogs#index'
+  # resources :blogs
+  # get 'blogs/:id', to: 'blogs#show'
+  # get 'blogs', to: 'blogs#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :users, only: [:index, :show] do
-    resources :blogs, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :blogs, only: %i[index show]
   end
   root 'users#index'
 end
