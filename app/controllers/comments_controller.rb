d510d2a7b7c6ee6a956721871af04a56e69a511f
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-02 18:43:21 +0100

    Add create method for comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,23 @@
 class CommentsController < ApplicationController
-  
+
   def new
     @comment = Comment.new
   end
+
+  def create
+    blog = Blog.find(params[:id])
+    comment = Comment.create(user_id: current_user.id, blog_id: blog.id, text: comment_params[:text])
+    if comment.save
+      flash[:notice] = 'Comment added'
+    else
+      flash[:alert] = 'Failed to add comment! Try again'
+    end
+    redirect_to user_blog_path(blog.user.id, blog.id)
+  end
   
+  private 
+
+  def comment_params
+    params.require(:data).permit(:text)
+  end
 end
