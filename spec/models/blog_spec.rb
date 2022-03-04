b6fa89fa2a5d8be365e1554b92b4e9e6d950f41d
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-04 09:12:21 +0100

    Add subject for blog model

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  
+  subject do
+    Blog.new(
+      user_id: 1,
+      title: 'Unit testing',
+      text: 'Unit testing is interesting. Though complicated!',
+      commentsCounter: 3,
+      likesCounter: 2
+    )
+  end
+  before { subject.save }
 end
