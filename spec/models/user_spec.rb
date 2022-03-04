7af8a903b3ebd480eb9dbab414af6e724369f965
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-04 08:53:50 +0100

    Add subject to create new user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,12 @@
 require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject do
+    User.new(
+      name: 'John',
+      photo: 'https://www.photo.com',
+      bio: 'A Teacher',
+      postsCounter: 2
+    )
+  end
+end
